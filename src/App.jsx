13ab437baed9c4b2d21d5ce5d90121b2f37dcbbd
MaximTertyshnyi <89603158+MaximTertyshnyi@users.noreply.github.com>
2022-04-16 23:21:35 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Routes ,Route } from 'react-router-dom';
 import { Homepage } from './pages/Homepage';
 import { Nvigation } from './pages/Nvigation';
 import { Music } from './pages/Musicpage';
+import { ITprofile } from './pages/ITpage';
+import { Aboutpage } from './pages/Aboutpage';
+
 
 
 
@@ -12,6 +15,8 @@ function App() {
     <Route path="/" element={<Nvigation/>}>
       <Route index element={<Homepage />} />
       <Route path="music" element={<Music />}></Route>
+      <Route path="it-profile" element={<ITprofile />}></Route>
+      <Route path="about" element={<Aboutpage />}></Route>
       {/* <Route path="about" element={<About />}></Route>
       <Route path="contacts" element={<p>Our contact</p>} />
             <Route path="team" element={<p>Our team</p>} /> */}
